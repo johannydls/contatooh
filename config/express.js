db4b97f68dfa60f5e25963650a8377b9a9f34f8e
Johanny de Lucena Santos <johanny.santos@ccc.ufcg.edu.br>
2018-07-06 13:01:13 -0300

    Fix port

--- config/express.js
+++ config/express.js
@@ -8,7 +8,7 @@ module.exports = () => {
     const app = express();
 
     //Definindo a porta do servidor
-    app.set('port', 8008);
+    app.set('port', process.env.PORT || 8008);
 
     //Middleware
     app.use(express.static('./public'));
