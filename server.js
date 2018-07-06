815d7899f41297f9a177f44ce27bbc290b4cd77f
Johanny de Lucena Santos <johanny.santos@ccc.ufcg.edu.br>
2018-07-06 12:57:26 -0300

    Fix database

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 //Lembrar de colocar no final o (), pois é uma função que está sendo exportada
 var app = require('./config/express')();
 
-require('./config/database.js')('mongodb://localhost:27017/contatooh');
+require('./config/database.js')('mongodb://contatooh:contatooh123@ds018248.mlab.com:18248/contatooh');
 
 http.createServer(app).listen(app.get('port'), () => {
     console.log('Express Server rodando em localhost:' + app.get('port'));
